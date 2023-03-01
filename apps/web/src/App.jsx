2be49832ac4363cc219704eeecaf1df0208318d6
Deepak Chaturvedi <deepakchaturvedifzd@gmail.com>
2023-03-01 18:17:15 +0530

    Update App.jsx

--- apps/web/src/App.jsx
+++ apps/web/src/App.jsx
@@ -1,25 +1,68 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { lazy, Suspense } from 'react';
+import {
+    BrowserRouter as Router,
+    Navigate,
+    Route,
+    Routes,
+} from 'react-router-dom';
+import { Loading } from 'ui';
 
-import CreateQuiz from './admin/CreateQuiz';
-import EditQuiz from './admin/EditQuiz';
 import AdminPageContainer from './admin/ListQuiz';
-import TakeQuizPreview from './admin/PreviewQuiz';
-import QuizReport from './admin/QuizReport';
-import ShowSubmission from './user/ShowSubmission';
 import TakeQuiz from './user/TakeQuiz';
+
+const QuizReport = lazy(() => import('./admin/QuizReport'));
+const EditQuiz = lazy(() => import('./admin/EditQuiz'));
+const TakeQuizPreview = lazy(() => import('./admin/PreviewQuiz'));
+const CreateQuiz = lazy(() => import('./admin/CreateQuiz'));
+const ShowSubmission = lazy(() => import('./user/ShowSubmission'));
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<AdminPageContainer />} />
-                <Route path="/take/:id" element={<TakeQuiz />} />
-                <Route path="/create/:id" element={<CreateQuiz />} />
-                <Route path="/submit/:id" element={<ShowSubmission />} />
-                <Route path="/preview/:id" element={<TakeQuizPreview />} />
-                <Route path="/quiz/report/:id" element={<QuizReport />} />
-                <Route path="/edit/:id" element={<EditQuiz />} />
+                <Route path="/" element={<Navigate replace to="/quiz" />} />
+                <Route path="/quiz" element={<AdminPageContainer />} />
+                <Route path="/quiz/:id" element={<TakeQuiz />} />
+                <Route
+                    path="/create/:id"
+                    element={
+                        <Suspense fallback={<Loading message="Loading..." />}>
+                            <CreateQuiz />
+                        </Suspense>
+                    }
+                />
+                <Route
+                    path="/submit/:id"
+                    element={
+                        <Suspense fallback={<Loading message="Loading..." />}>
+                            <ShowSubmission />
+                        </Suspense>
+                    }
+                />
+                <Route
+                    path="/preview/:id"
+                    element={
+                        <Suspense fallback={<Loading message="Loading..." />}>
+                            <TakeQuizPreview />
+                        </Suspense>
+                    }
+                />
+                <Route
+                    path="/quiz/report/:id"
+                    element={
+                        <Suspense fallback={<Loading message="Loading..." />}>
+                            <QuizReport />
+                        </Suspense>
+                    }
+                />
+                <Route
+                    path="/edit/:id"
+                    element={
+                        <Suspense fallback={<Loading message="Loading..." />}>
+                            <EditQuiz />
+                        </Suspense>
+                    }
+                />
             </Routes>
         </Router>
     );
