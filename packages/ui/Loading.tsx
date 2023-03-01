612d3db3c25db8ac23c110343778ba7465fd32a8
Deepak Chaturvedi <deepakchaturvedifzd@gmail.com>
2023-03-01 17:44:16 +0530

    Update Loading.tsx

--- packages/ui/Loading.tsx
+++ packages/ui/Loading.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Spinner from 'react-bootstrap/Spinner';
+import './style.css';
 
 type Props = {
     message: string;
@@ -7,9 +8,9 @@ type Props = {
 
 export const Loading: React.FC<Props> = ({ message }) => {
     return (
-        <>
+        <div className="loading-ui">
             <Spinner animation="grow" />
             <p>{message}</p>
-        </>
+        </div>
     );
 };
